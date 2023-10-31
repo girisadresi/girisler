e39bc180ac85c89d025d3bd093befa10f5a73cf3
gitlabpages <135068302+gitlabpages@users.noreply.github.com>
2023-11-01 02:06:15 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'girisler'
+copyright = '2023, Giriş Adresleri'
+author = 'girisadresi'
 
 release = '0.1'
 version = '0.1.0'
